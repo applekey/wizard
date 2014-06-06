52a80336a6f548bb03459216b568cd8063973a05
vincent <applekey@gmail.com>
2014-06-05 23:39:01 -0400

    blah

--- PyQt4app.py
+++ PyQt4app.py
@@ -1,19 +1,30 @@
-from PyQt4 import QtGui
+from PyQt4 import QtCore,QtGui
 from PyQt4.phonon import Phonon
 from wiz import Ui_WizardPage
 
 # create the main window class
 
 class ApplicationWindow(QtGui.QMainWindow):
+    wizard = None
+    a = None
+    b = None
+
     def __init__( self, parent = None ):
         super(ApplicationWindow, self).__init__(parent)
         self.setWindowTitle('Main Window')
-
+    def nextCalled(self,id):
+        self.a.calluser()
     def exportMovies( self ):
         """ Launches the export movies wizard. """
-        wizard = QtGui.QWizard(self)
-        wizard.addPage(Ui_WizardPage(wizard))
-        wizard.exec_()
+       
+        self.wizard = QtGui.QWizard(self)
+        self.a = Ui_WizardPage(self.wizard)
+        self.b = Ui_WizardPage(self.wizard)
+        self.wizard.addPage(self.a)
+        self.wizard.addPage(self.b)
+        self.wizard.currentIdChanged.connect(self.nextCalled)
+        self.wizard.exec_()
+        #wizard.setWindowFlags(QtCore.Qt.FramelessWindowHint )
 
 if ( __name__ == '__main__' ):
     # create the application if necessary
@@ -22,7 +33,6 @@ if ( __name__ == '__main__' ):
         app = QtGui.QApplication([])
     
     window = ApplicationWindow()
-   
     #window.show()
     window.exportMovies()
     
