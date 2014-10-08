3e8ad2f3bde268a1574ae39d11b50c54a1651bfd
vince <applekey@gmail.com>
2014-10-07 22:16:50 -0400

    plane cut is now a group

--- wizardV2/static/js/subControl.js
+++ wizardV2/static/js/subControl.js
@@ -126,6 +126,7 @@ var subControlTemplate = "<br>\
 							</div>\
 						</div>\
 					</div>"
+var subControlValuesPage2 = {mainStep: "Step 2 Progress"};
 var subControlValuesPage3 = {mainStep: "Step 3 Progress"};
 var subControlValuesPage4 = {mainStep: "Step 4 Progress"};
 var subControlValuesPage5 = {mainStep: "Step 5 Progress"};
