4bf1dcfb95923e9ac031e9bd804a8c31c0f37c10
vince <applekey@gmail.com>
2014-12-14 21:09:39 -0500

    fdas

--- wizardV2/static/js/subControl.js
+++ wizardV2/static/js/subControl.js
@@ -177,10 +177,12 @@ var subControlTemplate = "<h5> {{mainStep}}</h5>\
 var subControlValuesPage2 = {mainStep: "Step 2, Part 1 Progress"};
 var subControlValuesPage3 = {mainStep: "Step 2, Part 2 Progress"};
 var subControlValuesPage4 = {mainStep: "Step 4 Progress"};
-var subControlValuesPage5 = {mainStep: "Step 3 Progress"};
+var subControlValuesPage5 = { mainStep: "Step 3 Part 1 Progress" };
+var subControlValuesPage32 = { mainStep: "Step 3 Part 2 Progress" }
 var subControlValuesPage61 = {mainStep: "Step 6 part 1 Progress"};
 var subControlValuesPage62 = {mainStep: "Step 6 part 2 Progress"};
 var subControlValuesPage63 = {mainStep: "Step 6 part 3 Progress"};
-var subControlValuesPage7 = {mainStep: "Step 7 Progress"};
+var subControlValuesPage7 = { mainStep: "Step 7 Progress" };
+
 
 // 
