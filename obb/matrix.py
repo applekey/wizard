6b36a971cc5c4ba91ad66d07dc69818eb56867ac
vince <applekey@gmail.com>
2014-12-19 02:16:15 -0500

    fdsa

--- obb/matrix.py
+++ obb/matrix.py
@@ -1,14 +1,13 @@
 def tr(a):
     sum = 0
-    for i in range(a.stride):
-        sum = sum+ a[i,i]
+    for i in range(len(a)):
+        sum = sum+ a[i][i]
     return sum
 
 #def eigenVal(matrix):
 #    pass
 def dotp(a,b):
-    result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*b)] for X_row in a]
-    print result
+    return [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*b)] for X_row in a]
 def addp(a,b):
     return map(lambda x,y:map(lambda w,z:w+z,x,y), a,b)
 
@@ -22,15 +21,17 @@ def bocher3x3(matrix):
     A3 = dotp(A2,matrix)
     trA = tr(matrix)
     trA2= tr(A2)
-    trA2= tr(A3)
+    trA3= tr(A3)
     a1 = -tr(matrix)
-    a2 = -0.5*(dotp(a1,trA) + trA2)
-
+    a2 = -0.5*(a1*trA)
+    a3 = -1/3*(a2*trA+a1*trA2+trA3)
+    return a1,a2,a3
 
 #print gaussElimination(abc)
 
 #print dotp(abc,abc)
 
-print abc
-print '\n'
-print addp(abc,abc)
+#print abc
+#print '\n'
+#print addp(abc,abc)
+print bocher3x3(abc)
