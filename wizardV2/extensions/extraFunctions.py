fe91865cc497f901713cefc699ffa68687e27bc6
vince <applekey@gmail.com>
2015-01-05 20:30:45 -0500

    about to push everything to real repo

--- wizardV2/extensions/extraFunctions.py
+++ wizardV2/extensions/extraFunctions.py
@@ -11,109 +11,7 @@ import math
 import os
 import numpy.linalg as linalg
 
-
-
-def centerModel():
-    cwd =os.getcwd()
-    tmpDirectory = os.path.join(cwd,'tmp')
-    fileName = os.path.join(tmpDirectory,'tmp.obj')
-    xAvg,yAvg,zAvg,xRot,zRot,rotationVector = calculateEigenVectors(fileName,0)
-    remote = mmRemote()
-    remote.connect()
-    cmd  = mmapi.StoredCommands( )
-    result =  mm.to_scene_xyz(remote,xAvg,yAvg,zAvg)
-    xAvg = result[0]
-    yAvg = result[1]
-    zAvg = result[2]
-    cmd.AppendBeginToolCommand('transform')
-    cmd.AppendToolParameterCommand('translation',-xAvg,0,-zAvg)
-    cmd.AppendCompleteToolCommand('accept')
-    remote.runCommand(cmd)
-    remote.shutdown()
-    os.remove (fileName)
-    return True
-
-
-###HACK## want to sleep, fix later
-selectedObjects = None 
-def getSelectedObject():
-    remote = mmRemote()
-    remote.connect()
-    selectedObjects = mm.list_selected_objects(remote)
-    print selectedObjects
-    remote.shutdown()
-    jsonreturn =  json.dumps(selectedObjects)
-    return jsonreturn
-
-def selectObjects(data):
-    if data != None:
-        remote = mmRemote()
-        remote.connect()
-        mm.select_objects(remote,data)
-        remote.shutdown()
-        return True
-    else:
-        return False
-
-
- 
-
-
-def reOrientModel():
-    cwd =os.getcwd()
-    tmpDirectory = os.path.join(cwd,'tmp')
-    fileName = os.path.join(tmpDirectory,'tmp.obj')
-    xAvg,yAvg,zAvg,xRot,zRot,rotationVector = calculateEigenVectors(fileName,0)
-
-    ## make another 270 degree rotation about y axis
-    angle = math.pi/2
-    #yRotation = np.matrix([[math.cos(angle), 0,-math.sin(angle)], [0, 1.0,0],[math.sin(angle), 0,math.cos(angle)]])
-    xRotation =np.matrix([[1.0, 0,0], [0, m.cos(angle),-m.sin(angle)],[0, m.sin(angle),m.cos(angle)]])
-    zRotation =np.matrix([[math.cos(angle), -math.sin(angle),0], [math.sin(angle), math.cos(angle),0],[0, 0,1]]) 
-
-    ## calculate the det to see if transformation is affine
-
-    if xRot:
-        rotationVector = np.dot(xRotation,rotationVector)
-    elif zRot:
-        rotationVector = np.dot(zRotation,rotationVector)
-   
-    
-    rotation =[]
-    a=rotationVector.item((0,0))
-    b=rotationVector.item((0,1))
-    c=rotationVector.item((0,2)) 
-    d= rotationVector.item((1,0))
-    e =rotationVector.item((1,1))
-    f=rotationVector.item((1,2))  
-    g=rotationVector.item((2,0))
-    h=rotationVector.item((2,1))
-    i=rotationVector.item((2,2)) 
-    
-    remote = mmRemote()
-    remote.connect()
-    cmd  = mmapi.StoredCommands( )
-    result =  mm.to_scene_xyz(remote,xAvg,yAvg,zAvg)
-    xAvg = result[0]
-    yAvg = result[1]
-    zAvg = result[2]
-    
-    cmd.AppendBeginToolCommand('transform')
-    cmd.AppendToolParameterCommand('rotation',a,b,c,d,e,f,g,h,i)
-
-    cmd.AppendToolParameterCommand('translation',-xAvg,-yAvg,-zAvg)
-    cmd.AppendCompleteToolCommand('accept')
-    remote.runCommand(cmd)
-    remote.shutdown()
-
-    os.remove (fileName)
-    # delete tmp model
-
-    return True
-    #[r,phi,delta] =calculateRotationAngles(rotationVector)
-
-    #calculateMax(xRow,yRow,zRow,rotationVector)
-    ## be sure to delete tmp directory
+########################### File Management CALLS ##############################
 
 @meshWrapper
 def exportTempModel():
@@ -133,7 +31,6 @@ def exportTempModel():
 def importFile(fileLocation = None,folder=None):
     if fileLocation != None and folder != None:
         currentPath = os.path.join(os.getcwd(), folder,fileLocation)
-        print currentPath
         if os.path.isfile(currentPath):
             return MeshWrapper.importFigure(currentPath)
         else:
@@ -142,106 +39,6 @@ def importFile(fileLocation = None,folder=None):
         return MeshWrapper.importFile()
 def planeCut():
     return MeshWrapper.planecut()
-
-def deleteObjectByName(name):
-    remote = mmRemote()
-    remote.connect()
-    [state,id]= mm.find_object_by_name(remote,name)
-    objects = []
-    objects.append(id)
-    mm.delete_objects(remote,objects)
-    remote.shutdown()
-
-
-def hideObjectByName(name):
-    remote = mmRemote()
-    remote.connect()
-    try:
-        [state,id]= mm.find_object_by_name(remote,name)
-        cmd  = mmapi.StoredCommands()
-        cmd.AppendSceneCommand_SetHidden(id)
-        remote.runCommand(cmd)
-        remote.shutdown()
-    except:
-        remote.shutdown()
-
-def showObjectByName(name):
-    remote = mmRemote()
-    remote.connect()
-    [state,id]= mm.find_object_by_name(remote,name)
-    cmd  = mmapi.StoredCommands()
-    cmd.AppendSceneCommand_SetVisible(id)
-    remote.runCommand(cmd)
-    remote.shutdown()
-
-
-def getAllObjects():
-    remote = mmRemote()
-    remote.connect()
-    try:
-        objects= mm.list_objects(remote)
-        objectnames= []
-        for object in objects:
-            name = mm.get_object_name(remote, object)
-            objectnames.append(name)
-        remote.shutdown()
-        # convert to json
-        jsonreturn =  json.dumps(objectnames)
-        return jsonreturn
-    except:
-        remote.shutdown()
-        return False
-
-def selectObjectByName(objectName):
-    print objectName
-    remote = mmRemote()
-    remote.connect()
-    mm.select_object_by_name(remote,objectName)
-
-    remote.shutdown()
-
-def renameObjectByName(origName, newName):
-    remote = mmRemote()
-    remote.connect()
-    try:
-        cmd  = mmapi.StoredCommands()
-        if  origName == '*' : # this mean everthing
-            objects= mm.list_objects(remote)
-            for object in objects:
-                cmd.AppendSceneCommand_SetObjectName(object,newName)
-            remote.runCommand(cmd)
-        else:
-            [state,id]= mm.find_object_by_name(remote,origName)
-        
-            cmd.AppendSceneCommand_SetObjectName(id,newName)
-            remote.runCommand(cmd)
-        remote.shutdown()
-        return True
-    except:
-        remote.shutdown() 
-        return False
-
-def duplicate(partToDuplicate):
-    remote = mmRemote()
-    remote.connect()
-    try:
-         mm.select_object_by_name(remote,partToDuplicate)
-         cmd  = mmapi.StoredCommands()
-         cmd.AppendBeginToolCommand('duplicate')
-         remote.runCommand(cmd)
-         remote.shutdown()
-     
-         return True
-    except:
-        remote.shutdown() 
-        return False
-
-@meshWrapper
-def freeOrbit(x,y):
-     #cmd  = mmapi.StoredCommands()
-     #cmd.CameraControl_TurntableOrbit(x,y)
-     return None
-     #return cmd
 
 @meshWrapper
 def importFigure(fileName):
@@ -258,16 +55,51 @@ def importFigure(fileName):
     if found == True:
         return cmd
     else:
-        return None                
+        return None  
 
-    #remesh values
+def saveStep(stepArray):
+    step = 'wizTmp'
+    for s in stepArray:
+        step = step+ '_'+str(s)
+    step = step + '.mix'
+    saveLatest(step)
+
+   
+def loadStep(stepArray):
+    step = 'wizTmp'
+    for step in stepArray:
+        step = step+ '_'+str(step)
+    step = step + '.mix'
+    loadLatest(step)
+
+@meshWrapper
+def loadLatest(name):
+    cmd  = mmapi.StoredCommands()
+    currentDir = os.getcwd()
+    saveFile = currentDir+name
+    if os.path.isfile(saveFile):
+        cmd.AppendSceneCommand_OpenMixFile(saveFile)
+    return cmd
+
+@meshWrapper
+def saveLatest():
+    name ='save.mix'
+    cmd  = mmapi.StoredCommands()
+    currentDir = os.getcwd()
+    historyDirectory = 'history'
+    if not os.path.exists(historyDirectory):
+        os.makedirs(historyDirectory)
+    saveFile = os.path.join(currentDir,'history',name)
+    cmd.AppendSceneCommand_ExportMixFile(saveFile)
+    return cmd
+
+########################### BASIC API CALLS ####################################
+
+
 @meshWrapper
 def remesh(param,paramValue):
     cmd  = mmapi.StoredCommands()
     cmd.AppendBeginToolCommand('remesh')
-    #cmd.AppendToolParameterCommand('density',remesh)
-    #cmd.AppendToolParameterCommand('smooth',smooth) 
-    #cmd.AppendToolParameterCommand('normalThreshold',threshold) 
     if param == 1:
         param = 'density'
     elif param ==2:
@@ -437,12 +269,31 @@ def bubbleSmooth():
     cmd.AppendToolUtilityCommand('setPrimary','bubbleSmooth')
     return cmd
 
-@meshWrapper
-def colorView():
-    cmd  = mmapi.StoredCommands()
-    cmd.ViewControl_SetTriangleColorMode(1)
-    
-    return cmd
+
+
+
+########################### SCENE API CALLS ####################################
+
+def centerModel():
+    cwd =os.getcwd()
+    tmpDirectory = os.path.join(cwd,'tmp')
+    fileName = os.path.join(tmpDirectory,'tmp.obj')
+    xAvg,yAvg,zAvg,xRot,zRot,rotationVector = calculateEigenVectors(fileName,0)
+    remote = mmRemote()
+    remote.connect()
+    cmd  = mmapi.StoredCommands( )
+    result =  mm.to_scene_xyz(remote,xAvg,yAvg,zAvg)
+    xAvg = result[0]
+    yAvg = result[1]
+    zAvg = result[2]
+    cmd.AppendBeginToolCommand('transform')
+    cmd.AppendToolParameterCommand('translation',-xAvg,0,-zAvg)
+    cmd.AppendCompleteToolCommand('accept')
+    remote.runCommand(cmd)
+    remote.shutdown()
+    os.remove (fileName)
+    return True
+
 
 @meshWrapper
 def align():
@@ -508,61 +359,188 @@ def alignZCam(view):
    
  
     cmd.CameraControl_SetSpecificView(eye,target,up)
-    #cmd.CameraControl_RecenterView()
     return cmd
 
 
-def saveStep(stepArray):
-    step = 'wizTmp'
-    for s in stepArray:
-        step = step+ '_'+str(s)
-    step = step + '.mix'
-    saveLatest(step)
-
-   
-def loadStep(stepArray):
-    step = 'wizTmp'
-    for step in stepArray:
-        step = step+ '_'+str(step)
-    step = step + '.mix'
-    loadLatest(step)
-
-@meshWrapper
-def loadLatest(name):
-    cmd  = mmapi.StoredCommands()
-    currentDir = os.getcwd()
-    saveFile = currentDir+name
-    if os.path.isfile(saveFile):
-        cmd.AppendSceneCommand_OpenMixFile(saveFile)
-    return cmd
-
-@meshWrapper
-def saveLatest():
-    name ='save.mix'
-    cmd  = mmapi.StoredCommands()
-    currentDir = os.getcwd()
-    historyDirectory = 'history'
-    if not os.path.exists(historyDirectory):
-        os.makedirs(historyDirectory)
-    saveFile = os.path.join(currentDir,'history',name)
-    cmd.AppendSceneCommand_ExportMixFile(saveFile)
-    return cmd
+########################### MESHMIXER OBJECT API CALLS #########################
 
 
 
-def saveText(text):
-    f = open('notes.txt','w')
-    f.write(text)
-    f.close()
 
+###HACK## want to sleep, fix later
+selectedObjects = None 
+def getSelectedObject():
+    remote = mmRemote()
+    remote.connect()
+    selectedObjects = mm.list_selected_objects(remote)
+    remote.shutdown()
+    jsonreturn =  json.dumps(selectedObjects)
+    return jsonreturn
 
-def loadText():
-    if os.path.isfile('notes.txt'):
-        file = open('notes.txt', 'r')
-        return file.read()
+def selectObjects(data):
+    if data != None:
+        remote = mmRemote()
+        remote.connect()
+        mm.select_objects(remote,data)
+        remote.shutdown()
+        return True
     else:
-        return ''
+        return False
 
+
+def reOrientModel():
+    cwd =os.getcwd()
+    tmpDirectory = os.path.join(cwd,'tmp')
+    fileName = os.path.join(tmpDirectory,'tmp.obj')
+    xAvg,yAvg,zAvg,xRot,zRot,rotationVector = calculateEigenVectors(fileName,0)
+
+    ## make another 270 degree rotation about y axis
+    angle = math.pi/2
+    #yRotation = np.matrix([[math.cos(angle), 0,-math.sin(angle)], [0, 1.0,0],[math.sin(angle), 0,math.cos(angle)]])
+    xRotation =np.matrix([[1.0, 0,0], [0, m.cos(angle),-m.sin(angle)],[0, m.sin(angle),m.cos(angle)]])
+    zRotation =np.matrix([[math.cos(angle), -math.sin(angle),0], [math.sin(angle), math.cos(angle),0],[0, 0,1]]) 
+
+    if xRot:
+        rotationVector = np.dot(xRotation,rotationVector)
+    elif zRot:
+        rotationVector = np.dot(zRotation,rotationVector)
+   
+    
+    rotation =[]
+    a=rotationVector.item((0,0))
+    b=rotationVector.item((0,1))
+    c=rotationVector.item((0,2)) 
+    d= rotationVector.item((1,0))
+    e =rotationVector.item((1,1))
+    f=rotationVector.item((1,2))  
+    g=rotationVector.item((2,0))
+    h=rotationVector.item((2,1))
+    i=rotationVector.item((2,2)) 
+    
+    remote = mmRemote()
+    remote.connect()
+    cmd  = mmapi.StoredCommands( )
+    result =  mm.to_scene_xyz(remote,xAvg,yAvg,zAvg)
+    xAvg = result[0]
+    yAvg = result[1]
+    zAvg = result[2]
+    
+    cmd.AppendBeginToolCommand('transform')
+    cmd.AppendToolParameterCommand('rotation',a,b,c,d,e,f,g,h,i)
+
+    cmd.AppendToolParameterCommand('translation',-xAvg,-yAvg,-zAvg)
+    cmd.AppendCompleteToolCommand('accept')
+    remote.runCommand(cmd)
+    remote.shutdown()
+
+    os.remove (fileName)
+    # delete tmp model
+
+    return True
+
+def deleteObjectByName(name):
+    remote = mmRemote()
+    remote.connect()
+    [state,id]= mm.find_object_by_name(remote,name)
+    objects = []
+    objects.append(id)
+    mm.delete_objects(remote,objects)
+    remote.shutdown()
+
+
+def hideObjectByName(name):
+    remote = mmRemote()
+    remote.connect()
+    try:
+        [state,id]= mm.find_object_by_name(remote,name)
+        cmd  = mmapi.StoredCommands()
+        cmd.AppendSceneCommand_SetHidden(id)
+        remote.runCommand(cmd)
+        remote.shutdown()
+    except:
+        remote.shutdown()
+
+def showObjectByName(name):
+    remote = mmRemote()
+    remote.connect()
+    [state,id]= mm.find_object_by_name(remote,name)
+    cmd  = mmapi.StoredCommands()
+    cmd.AppendSceneCommand_SetVisible(id)
+    remote.runCommand(cmd)
+    remote.shutdown()
+
+
+def getAllObjects():
+    remote = mmRemote()
+    remote.connect()
+    try:
+        objects= mm.list_objects(remote)
+        objectnames= []
+        for object in objects:
+            name = mm.get_object_name(remote, object)
+            objectnames.append(name)
+        remote.shutdown()
+        # convert to json
+        jsonreturn =  json.dumps(objectnames)
+        return jsonreturn
+    except:
+        remote.shutdown()
+        return False
+
+def selectObjectByName(objectName):
+    remote = mmRemote()
+    remote.connect()
+    mm.select_object_by_name(remote,objectName)
+
+    remote.shutdown()
+
+def renameObjectByName(origName, newName):
+    remote = mmRemote()
+    remote.connect()
+    try:
+        cmd  = mmapi.StoredCommands()
+        if  origName == '*' : # this mean everthing
+            objects= mm.list_objects(remote)
+            for object in objects:
+                cmd.AppendSceneCommand_SetObjectName(object,newName)
+            remote.runCommand(cmd)
+        else:
+            [state,id]= mm.find_object_by_name(remote,origName)
+        
+            cmd.AppendSceneCommand_SetObjectName(id,newName)
+            remote.runCommand(cmd)
+        remote.shutdown()
+        return True
+    except:
+        remote.shutdown() 
+        return False
+
+def duplicate(partToDuplicate):
+    remote = mmRemote()
+    remote.connect()
+    try:
+         mm.select_object_by_name(remote,partToDuplicate)
+         cmd  = mmapi.StoredCommands()
+         cmd.AppendBeginToolCommand('duplicate')
+         remote.runCommand(cmd)
+         remote.shutdown()
+     
+         return True
+    except:
+        remote.shutdown() 
+        return False
+
+              
+
+@meshWrapper
+def colorView():
+    cmd  = mmapi.StoredCommands()
+    cmd.ViewControl_SetTriangleColorMode(1)
+    
+    return cmd
+
+
+########################### SOCKET BUILDER CODE #########################
 
 
 # [RMS] start the selection tool
